04722d2542c8a287cf00196c0b572a371e557a66
Peninah Midiwo <ninah.midiwo@gmail.com>
2018-09-21 05:48:44 +0300

    MISC Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :agents
+  devise_for :merchants
+  devise_for :partners
+  devise_for :resellers
+  devise_for :officers
+  devise_for :users
+  devise_for :administrators
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
 end
